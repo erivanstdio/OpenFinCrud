11565c6af1401d9754d4f57de0b955c7d88cb645
erivanstdio <erivanstdio@gmail.com>
2024-06-19 02:16:11 -0300

    improving utils

--- src/app/util/redirectTo.ts
+++ src/app/util/redirectTo.ts
@@ -1,6 +1,6 @@
 import { Router } from "@angular/router";
 
 export function redirectTo(router: Router, uri: string) {
-  router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+  router.navigateByUrl('/home', { skipLocationChange: true }).then(() => {
     router.navigate([uri])});
 }
